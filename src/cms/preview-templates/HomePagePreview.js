2f9f8d2060d0364e156a835b1157238e6dd09ad0
Lucho Suarez <luis.suarez@rrpartners.com>
2018-03-21 22:09:53 -0700

    Tryng to fix the home page query

---
+++ src/cms/preview-templates/HomePagePreview.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { HomePageTemplate } from '../../templates/home-page'
+
+const HomePagePreview = ({ entry, getAsset }) => {
+  const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
+  const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
+
+  const entryTestimonials = entry.getIn(['data', 'testimonials'])
+  const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
+
+  const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
+  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
+
+  return (
+    <HomePageTemplate
+      heading={entry.getIn(['data', 'heading'])}
+      subheading={entry.getIn(['data', 'subheading'])}
+      intro={{ blurbs }}
+      sllider={{
+        image1: {
+          image: getAsset(entry.getIn(['data', 'main', 'image1', 'image'])),
+          alt: entry.getIn(['data', 'main', 'image1', 'alt']),
+        },
+        image2: {
+          image: getAsset(entry.getIn(['data', 'main', 'image2', 'image'])),
+          alt: entry.getIn(['data', 'main', 'image2', 'alt']),
+        },
+        image3: {
+          image: getAsset(entry.getIn(['data', 'main', 'image3', 'image'])),
+          alt: entry.getIn(['data', 'main', 'image3', 'alt']),
+        },
+      }}
+    />
+  )
+}
+
+export default HomePagePreview
---
+++ src/templates/home-page.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import Pricing from '../components/Pricing'
+
+export const HomePageTemplate = ({ heading, subheading, intro, slider }) => (
+  <section className="section section--gradient">
+    <div className="container">
+      <div className="section">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="content" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <HomePageTemplate
+      heading={frontmatter.heading}
+      subHeading={frontmatter.subHeading}
+      slider={frontmatter.slider}
+      intro={frontmatter.intro}
+      main={frontmatter.main}
+    />
+  )
+}
+
+export const homePageQuery = graphql`
+  query HomePage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        heading
+        subheading
+        intro {
+          blurbs {
+            image
+            text
+          }
+        }
+        slider {
+          image1 {
+            alt
+            image
+          }
+          image2 {
+            alt
+            image
+          }
+          image3 {
+            alt
+            image
+          }
+        }
+      }
+    }
+  }
+`
