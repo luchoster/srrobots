a29f83c36a5759658b47a5e2a5295963ae951023
Lucho Suarez <luis.suarez@rrpartners.com>
2018-03-22 16:08:38 -0700

    adding mission and testimonials to the cms for the home page. Adding the html for the about page

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -5,20 +5,74 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
+    <main id="main" className="about-bg">
+      <section className="about-holder tc-padding">
+        <div className="container">
+          <div className="about-content has-layout">
+            <div className="row">
+              <div className="col-sm-6">
+                <div className="about-img-2" />
+              </div>
+              <div className="col-lg-6 col-md-6 col-sm-12">
+                <div className="about-text style-2">
+                  <h5>Story Teller, Painting</h5>
+                  <div className="kids-img has-layout">
+                    <ul>
+                      <li>
+                        <img
+                          src={require('../assets/imgs/kids-imgs/img-01.png')}
+                          alt=""
+                        />
+                      </li>
+                      <li>
+                        <img
+                          src={require('../assets/imgs/kids-imgs/img-02.png')}
+                          alt=""
+                        />
+                      </li>
+                      <li>
+                        <img
+                          src={require('../assets/imgs/kids-imgs/img-03.png')}
+                          alt=""
+                        />
+                      </li>
+                      <li>
+                        <img
+                          src={require('../assets/imgs/kids-imgs/img-04.png')}
+                          alt=""
+                        />
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="session-news curve-down style-2 has-layout">
+              <span className="scho-service-icon style-2">
+                <img
+                  src={require('../assets/imgs/school-services/img-1-1.png')}
+                  alt=""
+                />
+              </span>
+              <img src={require('../assets/imgs/about-img-2.jpg')} alt="" />
+              <div className="news-caption position-center-x">
+                <h4>Session 2017 Addmission Open</h4>
+                <p>
+                  Welcome to KG daycare, preschool, and kindergarten How to
+                  Enroll Your Child to a Class?
+                </p>
+              </div>
+              <div className="enroll-now">
+                <p>Get started today!</p>
+                <a className="tc-btn" href="https://shop.srrobots.com/">
+                  Buy Now
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </main>
   )
 }
 
