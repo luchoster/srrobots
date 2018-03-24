2fea82b8026bf4ebac90d39755ab5d8a0e9678e4
Lucho Suarez <luis.suarez@rrpartners.com>
2018-03-23 17:25:48 -0700

    Fixing the home slider to be responsive. Styling the offcanvas menu

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,12 +5,21 @@ import Link from 'gatsby-link'
 import {
   AppBar,
   Drawer,
+  List,
   ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
   IconButton,
   Typography,
   Toolbar,
 } from 'material-ui'
 import MenuIcon from 'material-ui-icons/Menu'
+import HomeIcon from 'material-ui-icons/Home'
+import ShoppingCartIcon from 'material-ui-icons/ShoppingCart'
+import InfoIcon from 'material-ui-icons/Info'
+import CameraIcon from 'material-ui-icons/PhotoCamera'
+import MessageIcon from 'material-ui-icons/Message'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import ReactTouchTap from 'react-tap-event-plugin'
@@ -31,7 +40,7 @@ class TemplateWrapper extends React.Component {
     return (
       <div>
         <Helmet title="Home | Sr. Robots" />
-        <AppBar position="fixed" className="app-bar d-lg-none">
+        <AppBar position="static" className="app-bar d-lg-none">
           <Toolbar>
             <IconButton
               onTouchTap={this._handleToggle}
@@ -46,20 +55,46 @@ class TemplateWrapper extends React.Component {
           </Toolbar>
         </AppBar>
         <Drawer open={this.state.open} onClose={this._handleToggle}>
+          <ListSubheader component="div">Main Menu</ListSubheader>
           <Link className="color-1" to="/">
-            <ListItem onTouchTap={this._closeDrawer}>home</ListItem>
+            <ListItem onTouchTap={this._closeDrawer} button>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="home" />
+            </ListItem>
           </Link>
           <Link className="color-4" to="/shop">
-            <ListItem onTouchTap={this._closeDrawer}>shop</ListItem>
+            <ListItem onTouchTap={this._closeDrawer} button>
+              <ListItemIcon>
+                <ShoppingCartIcon />
+              </ListItemIcon>
+              <ListItemText primary="shop" />
+            </ListItem>
           </Link>
           <Link className="color-3" to="/about">
-            <ListItem onTouchTap={this._closeDrawer}>about us</ListItem>
+            <ListItem onTouchTap={this._closeDrawer} button>
+              <ListItemIcon>
+                <InfoIcon />
+              </ListItemIcon>
+              <ListItemText primary="about us" />
+            </ListItem>
           </Link>
           <Link className="color-2" to="/gallery">
-            <ListItem onTouchTap={this._closeDrawer}>gallery</ListItem>
+            <ListItem onTouchTap={this._closeDrawer} button>
+              <ListItemIcon>
+                <CameraIcon />
+              </ListItemIcon>
+              <ListItemText primary="gallery" />
+            </ListItem>
           </Link>
           <Link className="color-7" to="/contact">
-            <ListItem onTouchTap={this._closeDrawer}>contact us</ListItem>
+            <ListItem onTouchTap={this._closeDrawer} button>
+              <ListItemIcon>
+                <MessageIcon />
+              </ListItemIcon>
+              <ListItemText primary="contact us" />
+            </ListItem>
           </Link>
         </Drawer>
         <Navbar />
