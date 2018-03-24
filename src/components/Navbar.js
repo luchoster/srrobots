c3da08a6fa65a9c584dca13be759db21835a1b3e
Lucho Suarez <luis.suarez@rrpartners.com>
2018-03-24 01:30:00 -0700

    Adding react-headroom for a nice animation in the header menu. Added contact page.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,51 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Headroom from 'react-headroom'
 
 import logo from '../assets/imgs/logo_small.png'
 
 const Navbar = () => (
-  <header id="header" className="header inner-header row d-none d-lg-flex">
-    {
-      <div className="top-bar col-sm-12">
-        <div className="container">
-          <div className="announcements">
-            <p>
-              <span />
-            </p>
-          </div>
+  <Headroom>
+    <header id="header" className="header inner-header row d-none d-lg-flex">
+      {
+        <div className="top-bar col-sm-12">
+          <div className="container">
+            <div className="announcements">
+              <p>
+                <span />
+              </p>
+            </div>
 
-          <div className="topbar-address">
-            {/*<a className="addmissions-open" href="#"><i className="icon-star-full" />Addmissions Open</a>*/}
-            <p>
-              <span>Contact Us</span>(702) 514-0607, info@srrobots.com
-            </p>
+            <div className="topbar-address">
+              {/*<a className="addmissions-open" href="#"><i className="icon-star-full" />Addmissions Open</a>*/}
+              <p>
+                <span>Contact Us</span>(702) 514-0607, info@srrobots.com
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    }
+      }
 
-    <div className="nav-holder col">
-      <div className="container">
-        <div className="p-relative has-layout">
-          <div className="logo">
-            <Link to="/">
-              <img src={logo} alt="" />
-            </Link>
-          </div>
+      <div className="nav-holder col">
+        <div className="container">
+          <div className="p-relative has-layout">
+            <div className="logo">
+              <Link to="/">
+                <img src={logo} alt="" />
+              </Link>
+            </div>
 
-          <nav className="navigation">
-            <a className="toggleMenu" href="#menu">
-              <i className="icon-navicon" />
-            </a>
-            <ul className="nav-list">
-              <li>
-                <Link className="color-1" to="/">
-                  home
-                </Link>
-              </li>
-              {/*<li>
+            <nav className="navigation">
+              <a className="toggleMenu" href="#menu">
+                <i className="icon-navicon" />
+              </a>
+              <ul className="nav-list">
+                <li>
+                  <Link className="color-1" to="/">
+                    home
+                  </Link>
+                </li>
+                {/*<li>
 									<a className="color-3" href="classes.html">classes</a>
 									<ul>
 										<li><a href="classes.html">classes</a></li>
@@ -60,22 +62,22 @@ const Navbar = () => (
 									</ul>
 								</li>
                 <li><a className="color-5" href="teams.html">teachers</a></li>*/}
-              <li>
-                <Link className="color-4" to="/shop">
-                  shop
-                </Link>
-              </li>
-              <li>
-                <Link className="color-3" to="/about">
-                  about us
-                </Link>
-              </li>
-              <li>
-                <Link className="color-2" to="/gallery">
-                  gallery
-                </Link>
-              </li>
-              {/*<li>
+                <li>
+                  <Link className="color-4" to="/shop">
+                    shop
+                  </Link>
+                </li>
+                <li>
+                  <Link className="color-3" to="/about">
+                    about us
+                  </Link>
+                </li>
+                <li>
+                  <Link className="color-2" to="/gallery">
+                    gallery
+                  </Link>
+                </li>
+                {/*<li>
 									<a className="color-2" href="#">pages</a>
 									<ul>
 										<li><a href="about.html">about us</a></li>
@@ -83,17 +85,18 @@ const Navbar = () => (
 										<li><a className="color-6" href="sceduled-events.html">timetable</a></li>
 									</ul>
                 </li>*/}
-              <li>
-                <Link className="color-7" to="/contact">
-                  contact us
-                </Link>
-              </li>
-            </ul>
-          </nav>
+                <li>
+                  <Link className="color-7" to="/contact">
+                    contact us
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
+    </header>
+  </Headroom>
 )
 
 export default Navbar
---
+++ src/pages/contact.js
@@ -0,0 +1,139 @@
+import React from 'react'
+import Hero from '../assets/imgs/inner-banners/img-08.jpg'
+
+const ContactPage = () => (
+  <React.Fragment>
+    <div
+      className="inner-banner contact text-center"
+      style={{
+        background: `url(${Hero}) center no-repeat`,
+        left: '0px',
+        right: ' 0px',
+      }}
+    >
+      <div className="container">
+        <div className="inner-heading">
+          <h2>
+            Reach us to us<br /> We’d love to hear from you
+          </h2>
+          <p>
+            If its a non urgent issue then you could also email us, we’ll get
+            back to you in 24 hours.
+          </p>
+        </div>
+      </div>
+    </div>
+    <main id="main">
+      {/* Contact */}
+      <section className="contact-holder gray-bg tc-padding-bottom">
+        <div className="container">
+          <div className="row">
+            {/* Content */}
+            <div className="col-sm-8">
+              <div className="content">
+                {/* Form Heading */}
+                <div className="form-heading">
+                  <h5>Drop your Message here</h5>
+                </div>
+                {/* Form Heading */}
+
+                {/* Form */}
+                <form
+                  className="comment-form contact"
+                  netlify
+                  method="POST"
+                  name="contact"
+                >
+                  <div className="form-group">
+                    <label>Your Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Johnny Five"
+                    />
+                    <i className="icon-user-circle-o" />
+                  </div>
+                  <div className="form-group">
+                    <label>Your Email</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="email@robots.com"
+                    />
+                    <i className="icon-envelope-o" />
+                  </div>
+                  <div className="form-group">
+                    <label>Phone Number</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="123 456 789"
+                    />
+                    <i className="icon-phone2" />
+                  </div>
+                  <div className="form-group">
+                    <textarea
+                      className="form-control"
+                      rows="10"
+                      placeholder="Enter Message"
+                    />
+                  </div>
+                  <div netlify-recaptcha />
+                  <button className="tc-btn full-with shadow-0">
+                    Submit Message
+                  </button>
+                </form>
+                {/* Form */}
+              </div>
+            </div>
+            {/* Content */}
+
+            {/* Aside */}
+            <aside className="col-sm-4">
+              <div className="address-figures tc-padding-top">
+                <ul className="address-list style-2">
+                  <li>
+                    <span>Conatct Us</span>
+                  </li>
+                  <li>
+                    <span>For Sales and Support</span>
+                    <p>(702) 514-0607</p>
+                  </li>
+                  <li>
+                    <span>Email Address</span>
+                    <p>support@srrobots.com</p>
+                  </li>
+                </ul>
+                <div className="tc-social-icons">
+                  <ul>
+                    <li>
+                      <a
+                        className="facebook"
+                        href="https://facebook.com/groups/SrRobots"
+                        target="_blank"
+                      >
+                        <i className="fa fa-facebook" />
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="twitter"
+                        href="https://twitter.com/sr_robots"
+                        target="_blank"
+                      >
+                        <i className="fa fa-twitter" />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </aside>
+            {/* Aside */}
+          </div>
+        </div>
+      </section>
+    </main>
+  </React.Fragment>
+)
+
+export default ContactPage
